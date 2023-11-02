e5b1e800182a1d50a8e967c40cd9051511be7cf3
Yudi-G <133056840+Yudi-G@users.noreply.github.com>
2023-11-02 17:15:50 +0200

    fixed up main for testing

--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -28,22 +28,22 @@ int main(){
     delete mstk;
     delete mainsteak;
 
-    std::cout << "do you want a\n 1.basic order\n2.main order";
+    std::cout << "do you want a\n 1.basic order\n2.main order\n";
     int orderType;
     std::cin >> orderType;
-    std::cout << "\n";
+    std::cout << "\n\n";
 
     if(orderType == 1){
-        std::cout << "do you want a\n 1.starter \n2.dessert";
+        std::cout << "do you want a\n 1.starter \n2.dessert\n";
         int stdes;
         std::cin >> stdes;
-        std::cout << "\n";
+        std::cout << "\n\n";
 
             if(stdes == 1){
-            std::cout << "do you want a\n 1.baked \n2.simmer";
+            std::cout << "do you want a\n 1.baked \n2.simmer\n";
             int ch;
             std::cin >> ch;
-            std::cout << "\n";
+            std::cout << "\n\n";
                 if(ch == 1){
                     BasicOrder b1("bread roll" , "baked" , "starter");
                     std::cout << b1.print() << std::endl;
@@ -54,10 +54,10 @@ int main(){
                 }
             }
             else{
-                std::cout << "do you want a\n 1.cold \n2.hot";
+                std::cout << "do you want a\n 1.cold \n2.hot\n";
                 int ch;
                 std::cin >> ch;
-                std::cout << "\n";
+                std::cout << "\n\n";
                     if(ch == 1){
                         BasicOrder b1("ice cream" , "cold" , "dessert");
                         std::cout << b1.print() << std::endl;
@@ -70,22 +70,22 @@ int main(){
 
     }
     else if(orderType == 2){
-        std::cout << "do you want a\n 1.protein \n2.sauce \n3.side \n4.veg";
+        std::cout << "do you want a\n 1.protein \n2.sauce \n3.side \n4.veg\n";
         int stdes;
         std::cin >> stdes;
-         std::cout << "\n";
+         std::cout << "\n\n";
 
             if(stdes == 1){
-                std::cout << "do you want a\n 1.grill \n2.fry";
+                std::cout << "do you want a\n 1.grill \n2.fry\n";
                 int ch;
                 std::cin >> ch;
-                 std::cout << "\n";
+                 std::cout << "\n\n";
 
                     if(ch == 1){
-                       std::cout << "do you want a\n 1.steak \n2.chicken";
+                       std::cout << "do you want a\n 1.steak \n2.chicken\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                             if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("steak", "grill", "main");
                                 MainOrder* mainsteak = new ProteinDecorator(mstk);
@@ -103,10 +103,10 @@ int main(){
                             
                     }
                     else{
-                    std::cout << "do you want a\n 1.fish \n2.ignore this";
+                    std::cout << "do you want a\n 1.fish \n2.ignore this\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                        if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("fish", "fry", "main");
                                 MainOrder* mainsteak = new ProteinDecorator(mstk);
@@ -120,19 +120,19 @@ int main(){
                     }
             }
             else if(stdes ==2) {
-                std::cout << "do you want a\n 1.simmer \n2.fry";
+                std::cout << "do you want a\n 1.simmer \n2.fry\n";
                 int ch;
                 std::cin >> ch;
-                 std::cout << "\n";
+                 std::cout << "\n\n";
 
                     if(ch == 1){
-                       std::cout << "do you want a\n 1.red wine \n2.ignore";
+                       std::cout << "do you want a\n 1.red wine \n2.ignore\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                             if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("red wine", "simmer", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SauceDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -143,20 +143,20 @@ int main(){
                             
                     }
                     else{
-                    std::cout << "do you want a\n 1.lemon sauce \n2.garlic sauce";
+                    std::cout << "do you want a\n 1.lemon sauce \n2.garlic sauce\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                        if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("lemon", "fry", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SauceDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
                             }
                         else{
                             BasicOrder* mstk = new BasicOrder("garlic", "fry", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SauceDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;  
@@ -164,19 +164,19 @@ int main(){
                     }
             }
             else if(stdes ==3) {
-                std::cout << "do you want a\n 1.boiled \n2.fried \n3.salad";
+                std::cout << "do you want a\n 1.boiled \n2.fried \n3.salad\n";
                 int ch;
                 std::cin >> ch;
-                 std::cout << "\n";
+                 std::cout << "\n\n";
 
                     if(ch == 1){
-                       std::cout << "do you want a\n 1.rice \n2.ignore";
+                       std::cout << "do you want a\n 1.rice \n2.ignore\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                             if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("rice", "boil", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SideDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -187,13 +187,13 @@ int main(){
                             
                     }
                     else if(ch==2){
-                    std::cout << "do you want a\n 1.chips \n2.ignore this";
+                    std::cout << "do you want a\n 1.chips \n2.ignore this\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                        if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("chips", "fry", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SideDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -203,13 +203,13 @@ int main(){
                         }
                     }
                     else if(ch==3){
-                    std::cout << "do you want a\n 1.salad \n2.ignore this";
+                    std::cout << "do you want a\n 1.salad \n2.ignore this\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                        if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("salad", "salad", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new SideDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -220,19 +220,19 @@ int main(){
                     }
             }
             else if(stdes ==4) {
-                std::cout << "do you want a\n 1.grill \n2.bake";
+                std::cout << "do you want a\n 1.grill \n2.bake\n";
                 int ch;
                 std::cin >> ch;
-                 std::cout << "\n";
+                 std::cout << "\n\n";
 
                     if(ch == 1){
-                       std::cout << "do you want a\n 1.mediterranean veg combo \n2.ignore";
+                       std::cout << "do you want a\n 1.mediterranean veg combo \n2.ignore\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                             if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("mediterranean", "grill", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new VegDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -243,20 +243,20 @@ int main(){
                             
                     }
                     else{
-                    std::cout << "do you want a\n 1.middle east \n2.south africa";
+                    std::cout << "do you want a\n 1.middle east \n2.south africa\n";
                        int ch;
                        std::cin >> ch;
-                        std::cout << "\n";
+                        std::cout << "\n\n";
                        if(ch==1){
                                 BasicOrder* mstk = new BasicOrder("middle east", "bake", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new VegDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
                             }
                         else{
                                 BasicOrder* mstk = new BasicOrder("south africa", "bake", "main");
-                                MainOrder* mainsteak = new ProteinDecorator(mstk);
+                                MainOrder* mainsteak = new VegDecorator(mstk);
                                 std::cout << mainsteak->print();
                                 delete mstk;
                                 delete mainsteak;
@@ -264,7 +264,7 @@ int main(){
                     }
             }
     }
-    else std::cout << "choose properly\n";
+    else std::cout << "choose properly\n\n";
 
 
 }
